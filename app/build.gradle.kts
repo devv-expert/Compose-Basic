0e80032c462599904c6a215ba925b9fa7ae02591
devv Expert <56317004+devv-expert@users.noreply.github.com>
2024-01-12 18:44:44 +0530

    Working on the Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,10 +53,10 @@ android {
 
 dependencies {
     /*implementation (fileTree(dir= 'libs', include: ['*.jar']))*/
-    implementation (files("libs/app-oneTap-debug.aar"))
+    /*implementation (files("libs/app-oneTap-debug.aar"))*/
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
@@ -74,5 +74,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     /*This will comvert the LiveData  to State in Compose*/
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
+    implementation("androidx.navigation:navigation-common:2.7.6")
+
+    /*Image Load Library*/
+    implementation("io.coil-kt:coil-compose:1.3.2")
 }
 
--- app/src/main/java/com/jetpack/composebasics/views/profileUI/ProfileUI.kt
+++ app/src/main/java/com/jetpack/composebasics/views/profileUI/ProfileUI.kt
@@ -1,22 +1,16 @@
-@file:Suppress("UNUSED_EXPRESSION")
-
 package com.jetpack.composebasics.views.profileUI
 
 import android.annotation.SuppressLint
-import android.graphics.Paint.Style
-import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -27,67 +21,64 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.ProvidedValue
-import androidx.compose.runtime.currentCompositionLocalContext
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.Placeable
-import androidx.compose.ui.layout.onPlaced
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.jetpack.composebasics.R
-import com.jetpack.composebasics.dataClass.UserData
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
+import com.jetpack.composebasics.dataClass.UserProfile
 import com.jetpack.composebasics.dataClass.userProfiles
 import com.jetpack.composebasics.views.profileUI.ui.theme.MyTheme
-import com.thalesgroup.tshpaysample.init.TshInit
-import com.thalesgroup.tshpaysample.payment.PaymentActivity
-import com.thalesgroup.tshpaysample.payment.TshPaymentData
-import java.security.Provider
 
 class ProfileUI : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MainScreen()
+                UserList()
             }
         }
     }
 }
 
- @Preview(showBackground = true)
+@Composable
+fun UserApplication() {
+
+}
+
+@Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
-    MainScreen()
+    UserList()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserProfilePreview() {
+    UserProfile()
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MainScreen(userData: List<UserData> = userProfiles) {
+fun UserList(userData: List<UserProfile> = userProfiles) {
     Scaffold(topBar = { AppBar() }) {
         Surface(
             modifier = Modifier.fillMaxSize(),
@@ -102,13 +93,10 @@ fun MainScreen(userData: List<UserData> = userProfiles) {
 
         }
     }
-
-    TshInit
-    PaymentActivity()
 }
 
 @Composable
-fun ProfileCard(userData: UserData) {
+fun ProfileCard(userData: UserProfile) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -122,44 +110,44 @@ fun ProfileCard(userData: UserData) {
         Row(
             modifier = Modifier.wrapContentSize()
         ) {
-            ProfilePicture(userData.userImageId, userData.isActive)
-            ProfileContent(userData.userName, userData.isActive)
+            ProfilePicture(userData.userImage, userData.status, 70.dp)
+            ProfileContent(userData.name, userData.status, Alignment.Start)
         }
     }
 }
 
 @Composable
-fun ProfilePicture(userImageId: Int, isActiveStatus: Boolean) {
+fun ProfilePicture(imageURL: String, isActiveStatus: Boolean, imageSize: Dp) {
     Card(
-        shape = CircleShape,
-        border = BorderStroke(
-            4.dp,
-            color = if (isActiveStatus) Color.Green else Color.Red
-        ),
-        modifier = Modifier.padding(15.dp)
+        shape = CircleShape, border = BorderStroke(
+            4.dp, color = if (isActiveStatus) Color.Green else Color.Red
+        ), modifier = Modifier.padding(15.dp)
     ) {
         Image(
-            painter = painterResource(id = userImageId),
-            contentDescription = "",
-            modifier = Modifier.size(75.dp),
-            contentScale = ContentScale.Crop
+            painter = rememberImagePainter(
+                data = imageURL,
+                builder = {
+                    transformations(CircleCropTransformation())
+                }
+            ),
+            modifier = Modifier.size(imageSize),
+            contentDescription = "Profile picture"
         )
     }
-
 }
 
 @Composable
-fun ProfileContent(userName: String, isActiveStatus: Boolean) {
-
+fun ProfileContent(userName: String, isActiveStatus: Boolean, alignment: Alignment.Horizontal) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .padding(8.dp),
+        horizontalAlignment = alignment
     ) {
         Text(
             text = userName,
             modifier = Modifier.padding(top = 5.dp),
-            style = MaterialTheme.typography.headlineMedium
+            style = MaterialTheme.typography.headlineSmall
         )
         Text(
             text = if (isActiveStatus) "Active" else "offline",
@@ -167,22 +155,41 @@ fun ProfileContent(userName: String, isActiveStatus: Boolean) {
             style = MaterialTheme.typography.titleMedium
         )
     }
-
 }
 
 @ExperimentalMaterial3Api
 @Composable
 fun AppBar() {
     val appBarTitle = "Top App Bar"
-    TopAppBar(
-        title = { Text(text = appBarTitle) },
-        navigationIcon = {
-            Icon(
-                imageVector = Icons.Default.Home,
-                contentDescription = "App Bar",
-                modifier = Modifier.padding(horizontal = 10.dp)
-            )
-        },
-        colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.primary)
+    TopAppBar(title = { Text(text = appBarTitle) }, navigationIcon = {
+        Icon(
+            imageVector = Icons.Default.Home,
+            contentDescription = "App Bar",
+            modifier = Modifier.padding(horizontal = 10.dp)
+        )
+    }, colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.primary)
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun UserProfile(userProfile: UserProfile = userProfiles[0]) {
+    Scaffold(topBar = { AppBar() }) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 65.dp),
+            color = MaterialTheme.colorScheme.surfaceColorAtElevation(5.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                ProfilePicture(userProfile.userImage, userProfile.status, 240.dp)
+                ProfileContent(userProfile.name, userProfile.status, Alignment.CenterHorizontally)
+            }
+        }
+    }
 }
